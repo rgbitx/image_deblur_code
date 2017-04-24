d6c42ff77ad53854b9007db93b3a4e3913a872e3
rgbitx <rgbitx@163.com>
2017-04-24 18:06:10 +0800

    clear

--- 13_camera_shake_paul/make_inset_axes.m
+++
@@ -1,4 +0,0 @@
-
-pos = get(gca,'Position');
-axes('Position',[0.005 0.00 0 0] + [1 1 0.4 0.4].*pos);
-%set(gca,'Color','w');
--- My_PatchSelection/resizeImage.m
+++
@@ -1,4 +0,0 @@
-I = imread('image1.jpg');
-Iscale = imresize(I,0.5);
-imshow(Iscale)
-imwrite(Iscale,'image1_1.png');
