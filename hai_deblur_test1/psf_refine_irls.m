f5ec55eb6bea35f99238243ef7912463296db5bd
rgbitx <rgbitx@163.com>
2017-05-08 19:15:47 +0800

    refine update

--- hai_deblur_test1/psf_refine_irls.m
+++ hai_deblur_test1/psf_refine_irls.m
@@ -16,15 +16,18 @@ function psf = psf_refine_irls(blurred_x, blurred_y, latent_x, latent_y, s_, wei
     p.lambda = weight;
     p.s_ = s_;
 
-    psf = ones(psf_size) / prod(psf_size);
-    
-    for iter = 1:1
+%     psf = ones(psf_size) / prod(psf_size);
+    psf = zeros(psf_size);
+
+    for iter = 1:3
         psf_prev = psf;
-        p.weights = p.lambda * (max(norm(psf_prev,1),1e-5)) * p.s_;                
+        p.weights = p.lambda  * p.s_ / (max(norm(psf_prev,1),1e-5));                
         psf = conjgrad(psf, b, 20, 1e-5, @compute_Ax, p);
 
         psf(psf < max(psf(:))*0.05) = 0;
         psf = psf / sum(psf(:));
+        
+        figure,imagesc(psf);        
     end
 end
 
